a59bbddd5087fc32cad209a4d91fb791a5753e75
Ken Howard <ken@kenhoward.dev>
2023-12-07 23:35:27 -0800

    chore: add lint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/eslint-recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["./tsconfig.eslint.json", "./packages/*/tsconfig.json"],
+  },
+  plugins: ["@typescript-eslint"],
+  root: true,
+  ignorePatterns: ["dist", ".eslintrc.js"],
+  rules: {
+    "no-console": 2, // Remember, this means error!
+  },
+  overrides: [
+    {
+      files: ["./packages/**/__tests__/*.ts"],
+      rules: {
+        "@typescript-eslint/no-unsafe-call": "off",
+        "@typescript-eslint/no-unsafe-member-access": "off",
+      },
+    },
+  ],
+};
