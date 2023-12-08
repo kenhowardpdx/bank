fbd82add8e1ac9eacb302d281017665645530d89
Ken Howard <ken@kenhoward.dev>
2023-12-08 15:27:14 -0800

    feat: format amount

---
+++ packages/server/src/api/v1/__tests__/amount.test.ts
@@ -0,0 +1,28 @@
+import { Amount } from "../amount";
+
+interface TestData {
+  input: string;
+  float: number;
+  formatted: string;
+}
+describe("Amount", () => {
+  test.each([
+    {
+      input: "15.00",
+      float: 15,
+      formatted: "$15.00",
+    },
+    {
+      input: "$5.15",
+      float: 5.15,
+      formatted: "$5.15",
+    },
+  ])(
+    "amount ($input) as float > $float; formatted: $formatted",
+    ({ input, float, formatted }: TestData) => {
+      const a = new Amount(input);
+      expect(a.amount).toEqual(float);
+      expect(a.toString()).toEqual(formatted);
+    },
+  );
+});
---
+++ packages/server/src/api/v1/amount.ts
@@ -0,0 +1,25 @@
+type Money = `${"$" | ""}${number}.${number}`;
+
+class Amount {
+  static format = (amount: number) => {
+    const formatter = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+
+    return formatter.format(amount) as Money;
+  };
+  private _amount: number;
+  constructor(amount: string) {
+    const amt = parseFloat(amount.replace("$", ""));
+    this._amount = amt;
+  }
+  public get amount() {
+    return this._amount;
+  }
+  public toString() {
+    return Amount.format(this._amount);
+  }
+}
+
+export { Amount };
