d6d00beac36d4f5c1708c6c4f9aadbb40896adc0
Ken Howard <ken@kenhoward.dev>
2024-01-02 22:20:59 -0800

    docs: deploy client

---
+++ docs/helpers/date.d.ts
@@ -0,0 +1,3 @@
+export declare const formatDate: (d: Date) => `${number}-${number}-${number}`;
+export declare const getFutureDate: (d: Date, addDays: number) => Date;
+export declare const normalizeDate: (d: Date) => string;
