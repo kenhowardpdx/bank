7531cef977ca2cff0a0711e5530ebaf57faa1935
Ken Howard <ken@kenhoward.dev>
2024-05-19 09:17:33 -0700

    fix: sort

--- apps/client/src/helpers/sort.ts
+++ apps/client/src/helpers/sort.ts
@@ -7,14 +7,19 @@ export const sort = (o: unknown, ...keys: Array<string>): Array<POJO> => {
 
   keys.forEach((key) =>
     copy.sort((a, b) => {
+      if (key.toLowerCase().includes("date")) {
+        const dateA = new Date(a[key] as string);
+        const dateB = new Date(b[key] as string);
+        if (dateA > dateB) {
+          return 1;
+        }
+        if (dateA < dateB) {
+          return -1;
+        }
+        return 0;
+      }
       if (typeof a[key] === "string") {
         return (<string>a[key]).localeCompare(<string>b[key]);
-      }
-      if (a[key] instanceof Date) {
-        return (
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          (new Date(a[key] as Date) as any) - (new Date(b[key] as Date) as any)
-        );
       }
       if (typeof a[key] === "number") {
         return <number>a[key] - <number>b[key];
